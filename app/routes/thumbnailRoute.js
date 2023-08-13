e375c678d87016b19f2977bd61b623c91aab4791
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2023-08-13 20:10:32 +0700

    update

--- app/routes/thumbnailRoute.js
+++ app/routes/thumbnailRoute.js
@@ -4,7 +4,7 @@ const thumbnailController = require('../controllers/thumbnailController');
 const router = express.Router();
 
 router.get('/', thumbnailController.getThumbnail);
-router.get('/', thumbnailController.getThumbnaiById);
+router.get('/video', thumbnailController.getThumbnaiById);
 router.post('/', thumbnailController.createThumbnail);
 
 module.exports = router;
